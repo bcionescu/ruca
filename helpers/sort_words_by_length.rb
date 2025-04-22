5098fa5761f09284ca6c72e83ca5206839c120b2
Cristian Ionescu <elevens_newtons0f@icloud.com>
2025-04-22 20:01:04 +0100

    Update sort_words_by_length.rb
    Adjusted paths.

--- helpers/sort_words_by_length.rb
+++ helpers/sort_words_by_length.rb
@@ -1,10 +1,12 @@
 def sort_words_by_length
-  file_path = 'data/words.txt'
-  lines = File.readlines(file_path)
+  root = File.expand_path("../../", __FILE__)
+  
+  words_path = "#{root}/data/words.txt"
+  lines = File.readlines(words_path)
 
   sorted_lines = lines.sort_by(&:length)
 
-  File.open('data/words.txt', 'w') do |file|
+  File.open(words_path, "w") do |file|
     sorted_lines.each { |line| file.puts(line) }
   end
 end
