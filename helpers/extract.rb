7e6d08b1af6dbb24da6f1072643d63b95a846186
Cristian Ionescu <elevens_newtons0f@icloud.com>
2025-04-23 22:43:56 +0100

    Update extract.rb
    Improving the code.

--- helpers/extract.rb
+++ helpers/extract.rb
@@ -1,34 +1,37 @@
-require_relative "integrity.rb"
+def extract
 
-root = File.expand_path("../../", __FILE__)
+  require_relative "integrity.rb"
 
-ruca_file = "#{root}/files/output.ruca"
-mappers   = "#{root}/data/mappers.txt"
-source    = "#{root}/files/source.txt"
-output    = "#{root}/files/extracted.txt"
+  root = File.expand_path("../../", __FILE__)
 
-dictionary = {}
+  ruca_file = "#{root}/files/output.ruca"
+  mappers   = "#{root}/data/mappers.txt"
+  source    = "#{root}/files/source.txt"
+  output    = "#{root}/files/extracted.txt"
 
-File.open(mappers, "r") do |mappers|
-  mappers.readlines.each do |line|
-    words = line.split
-    key, value = words[0], words[1]
-    dictionary[key] = value
+  dictionary = {}
+
+  File.open(mappers, "r") do |mappers|
+    mappers.readlines.each do |line|
+      words = line.split
+      key, value = words[0], words[1]
+      dictionary[key] = value
+    end
   end
+
+  File.open(ruca_file, "r") do |text|
+    content = text.read
+
+    dictionary.each do |key, value|
+      content.gsub!("<#{value}>", key)
+      content.gsub!("^#{value}>", key.capitalize)
+      content.gsub!("^#{value}^", key.upcase)
+    end
+
+    File.open(output, "w") do |file|
+      file.write(content)
+    end
+  end
+
+  integrity(source, output)
 end
-
-File.open(ruca_file, "r") do |text|
-  content = text.read
-
-  dictionary.each do |key, value|
-    content.gsub!("<#{value}>", key)
-    content.gsub!("^#{value}>", key.capitalize)
-    content.gsub!("^#{value}^", key.upcase)
-  end
-
-  File.open(output, "w") do |file|
-    file.write(content)
-  end
-end
-
-integrity(source, output)
