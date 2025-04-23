12c7ceb5dd7b8ecce003dcce8ca2718215371be9
Cristian Ionescu <elevens_newtons0f@icloud.com>
2025-04-23 22:48:55 +0100

    Create compress.rb

---
+++ helpers/compress.rb
@@ -0,0 +1,46 @@
+def compress
+      
+  dictionary = {}
+
+  File.open("data/mappers.txt", "r") do |text|
+    text.readlines.each do |line|
+      words = line.split
+      key, value = words
+      dictionary[key] = value
+    end
+  end
+
+  processed_content = ""
+  File.open("files/source.txt", "r") do |text|
+    text.readlines.each do |line|
+      words = line.split(/(\W+)/)
+      transformed_words = words.map do |word|
+
+        if word.match?(/\w+/)
+          lowercase_word = word.downcase
+          
+          if dictionary.has_key?(lowercase_word)
+            replacement = dictionary[lowercase_word]
+            
+            case word
+            when word.upcase then     "^#{replacement}^"
+            when word.downcase then   "<#{replacement}>"
+            when word.capitalize then "^#{replacement}>"
+            else replacement
+            end
+          else
+            word
+          end
+        else
+          word
+        end
+      end
+
+      processed_content += transformed_words.join
+    end
+  end
+
+  File.open("files/output.ruca", "w") do |file|
+    file.write(processed_content)
+  end
+end
