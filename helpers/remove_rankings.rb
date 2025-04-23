2d674bbb94229d092b3a64e95d33d45a5b8c4e46
Cristian Ionescu <elevens_newtons0f@icloud.com>
2025-04-23 22:42:53 +0100

    Update remove_rankings.rb
    Improving the code.

--- helpers/remove_rankings.rb
+++ helpers/remove_rankings.rb
@@ -1,12 +1,11 @@
 def remove_rankings
   root = File.expand_path("../../", __FILE__)
 
-  sorted_path = "#{root}/data/sorted.txt"
   words_path  = "#{root}/data/words.txt"
   
   $new_text = ""
 
-  File.open(sorted_path, "r") do |text|
+  File.open(words_path, "r") do |text|
     text.readlines.each do |line|
       line = line.chomp.gsub(/ .*/, "")
       $new_text += "#{line}\n"
@@ -17,4 +16,3 @@ def remove_rankings
     file.write($new_text)
   end
 end
-
